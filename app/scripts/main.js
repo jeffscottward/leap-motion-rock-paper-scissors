285a6077229f6bee5e3173bfafb46dbc3d1af762
jeff ward <jsward.17@gmail.com>
2014-05-12 19:38:40 -0400

    basic web app setup with leaps plugins and PN sdk

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+// Protect Global Namespace
+// Execute Function Immediately
+(function(){
+
+  // Setup new PubNub Object c w/ auth Keys
+   var pubnub = PUBNUB.init({
+       publish_key   : 'pub-c-d1d43525-54ef-4f9d-a6d3-909d601cc3f4',
+       subscribe_key : 'sub-c-2885c358-da26-11e3-bf22-02ee2ddab7fe'
+   });
+
+   // Set up this environment's client subscriber instance
+   pubnub.subscribe({
+       channel : "hello_world",
+       message : function(m){ alert(m) },
+       connect : publish
+   });
+
+   // Function to push a a message
+   function publish() {
+       pubnub.publish({
+           channel : "hello_world",
+           message : "Hi."
+       })
+   }
+
+ })();
