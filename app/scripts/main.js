b0f357578a161756cbe368661f0bdacaee0b22b6
jeff ward <jsward.17@gmail.com>
2014-05-14 23:12:06 -0400

    fix some stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,27 +3,27 @@
 (function(){
 
   // Setup new PubNub Object c w/ auth Keys
-  var pubnub = PUBNUB.init({
-    publish_key   : 'pub-c-d1d43525-54ef-4f9d-a6d3-909d601cc3f4',
-    subscribe_key : 'sub-c-2885c358-da26-11e3-bf22-02ee2ddab7fe'
-  });
-
-  // Set up this environment's client subscriber instance
-  pubnub.subscribe({
-    channel : "RPSchannel",
-    message : function(msg) {
-      console.log(msg)
-    },
-    connect : publish
-  });
-
-  // Function to push a a message
-  function publish() {
-    pubnub.publish({
-      channel : "RPSchannel",
-      message : "A challenger appears! Get ready to fight!"
-    });
-  }
+  // var pubnub = PUBNUB.init({
+  //   publish_key   : 'pub-c-d1d43525-54ef-4f9d-a6d3-909d601cc3f4',
+  //   subscribe_key : 'sub-c-2885c358-da26-11e3-bf22-02ee2ddab7fe'
+  // });
+  //
+  // // Set up this environment's client subscriber instance
+  // pubnub.subscribe({
+  //   channel : "RPSchannel",
+  //   message : function(msg) {
+  //     console.log(msg)
+  //   },
+  //   connect : publish
+  // });
+  //
+  // // Function to push a a message
+  // function publish() {
+  //   pubnub.publish({
+  //     channel : "RPSchannel",
+  //     message : "A challenger appears! Get ready to fight!"
+  //   });
+  // }
 
   var RPSapp = {};
       RPSapp.fingerCount;
