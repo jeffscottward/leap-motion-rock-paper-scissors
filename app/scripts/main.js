a0038f03fa1c09443c0845573f000d9a90f900ad
jeff ward <jsward.17@gmail.com>
2014-05-20 17:14:54 -0400

    refactor and sync via hashes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,52 +12,36 @@
       };
       RPSapp.pubNub; // PubNub API
       RPSapp.thisUserID;
+      RPSapp.channelID = window.location.hash !== "" ? window.location.hash.split('#')[1] : createGameId();
       RPSapp.gameRunning = true;
       RPSapp.clock = $('#rps-clock').FlipClock(3,{autoStart: true,countdown: true});
-      RPSapp.clockTime = RPSapp.clock.getTime();
       RPSapp.init = function (){
         initPubNub();
         getCurrentChannelStatus();
         initLeap();
-        initClock();
+        // roundOverDetection();
       }
 
-  // For Testing only
-  function callStackReporter(funcName) {
-    console.log(funcName);
-  }
+  // TODO: Replace Clock w/ TimeStamp via Presence API
+  // http://www.pubnub.com/docs/javascript/overview/presence.html#_using_presence_api
 
-  function initClock(){
-    callStackReporter('initClock');
+  // On initial connection push a message
+  // To broadcast a user joining
+  function onChannelJoin() {
+    callStackReporter('onChannelJoin()');
 
-    var secondTickerShadow = $('.flip-clock-divider.seconds + .flip + .flip [data-digit="0"] .down');
-        secondTickerShadow.on('webkitAnimationEnd',function(e){
-          RPSapp.gameRunning = false;
-          console.log('Round Over! You chose ' + RPSapp.playerChoice + '!');
-        });
-  }
-
-  function updateGlobalTimer() {
-    callStackReporter('updateGlobalTimer');
-
-    // console.log();
-
+    // Broadcast a new user has joined
     RPSapp.pubNub.publish({
-      channel : "rps_channel",
+      channel : RPSapp.channelID,
       message : {
-        currentTime: RPSapp.clockTime.factory.time.time.toString()
+        newUser: RPSapp.thisUserID,
+        bodyMsg: "The challenger " + RPSapp.thisUserID + " appears! Get ready to fight!"
       }
     });
-  }
 
+    console.log( RPSapp.thisUserID + ' has joined ' + RPSapp.channelID );
 
-  // Build new user tempalte and inject
-  function injectUser(userId) {
-    var externalUserTemplate = "<li data-role=externalUser id='" + userId + "'><div class='rock'>" +
-    "<h1>USER " + userId + "</h1><img src='/images/rps-rock.png'/></div><div class='paper'>" +
-    "<h1>USER " + userId + "</h1><img src='/images/rps-paper.png'/></div><div class='scissors'>" +
-    "<h1>USER " + userId + "</h1><img src='/images/rps-scissors.png'/></div></li>";
-    RPSapp.displayArea.playerList.append(externalUserTemplate);
+    injectUser(RPSapp.thisUserID);
   }
 
   //Setup new PubNub Object c w/ auth Keys
@@ -69,52 +53,74 @@
     RPSapp.pubNub = PUBNUB.init({
       publish_key   : 'pub-c-d1d43525-54ef-4f9d-a6d3-909d601cc3f4',
       subscribe_key : 'sub-c-2885c358-da26-11e3-bf22-02ee2ddab7fe',
-      uuid: assignUniqueID()
+      uuid: assignUniqueUserID()
     });
 
-    // Subscribe to channel and handle messages
+    // Subscribe to channel
+    initSubscribe();
+  }
+
+  function initSubscribe(){
     RPSapp.pubNub.subscribe({
-      channel : "rps_channel",
-      message : function(msg) {
-
-        var newUserID = msg.newUser;
-        var user = msg.user;
-        var choiceUpdate = msg.choiceUpdate;
-
-        // If a new user message recieved
-        // inject user
-        if (newUserID) {injectUser(userId)}
-
-        // If a choice update message recieved
-        // update the ID DOM
-        if(choiceUpdate) {$('#' + user).attr('class',choiceUpdate);}
-
+      channel : RPSapp.channelID,
+      presence: function(presence){
+        // console.log("presence: "); console.dir(presence)
       },
+      message : function(msg){channelSubscriptionMsgHandler(msg)},
       connect : onChannelJoin()
     });
-
   }
 
   // Create unique ID for this client user
-  function assignUniqueID() {
-    callStackReporter('assignUniqueID()');
-    RPSapp.thisUserID = PUBNUB.uuid();
+  function assignUniqueUserID() {
+    callStackReporter('assignUniqueUserID()');
+
+    // Retrieve from Localstorage first
+    var localID = localStorage.getItem("RPSuuid");
+
+    // console.log(localID);
+
+    // If there is an ID
+    if( localID !== "" &&
+        localID !== undefined &&
+        localID !== null ) {
+
+          // Assign it to app runtime
+          RPSapp.thisUserID = localID;
+        } else {
+
+            // Create one and assign it to app runtime
+            localStorage.setItem("RPSuuid", PUBNUB.uuid());
+            RPSapp.thisUserID = PUBNUB.uuid();
+        }
+
     return RPSapp.thisUserID;
   }
 
-  // On initial connection push a message
-  // To broadcast a user joining
-  function onChannelJoin() {
-    callStackReporter('onChannelJoin()');
+  // Build new user tempalte and inject
+  function injectUser(userId) {
+    var externalUserTemplate = "<li data-role=externalUser id='" + userId + "'><div class='rock'>" +
+    "<h1>USER " + userId + "</h1><img src='/images/rps-rock.png'/></div><div class='paper'>" +
+    "<h1>USER " + userId + "</h1><img src='/images/rps-paper.png'/></div><div class='scissors'>" +
+    "<h1>USER " + userId + "</h1><img src='/images/rps-scissors.png'/></div></li>";
+    RPSapp.displayArea.playerList.append(externalUserTemplate);
+  }
 
-    // Broadcast a new user has joined
-    RPSapp.pubNub.publish({
-      channel : "rps_channel",
-      message : {
-        newUser: RPSapp.thisUserID,
-        bodyMsg: "The challenger " + RPSapp.thisUserID + " appears! Get ready to fight!"
-      }
-    });
+  // Handle all messages
+  function channelSubscriptionMsgHandler(msg){
+
+    var newUserID = msg.newUser;
+    var user = msg.user;
+    var choiceUpdate = msg.choiceUpdate;
+
+    // If a new user message recieved
+    // inject user
+    if (newUserID !== RPSapp.thisUserID) {injectUser(newUserID)}
+
+    // If a choice update message recieved
+    // update the ID DOM
+    if(choiceUpdate) {$('#' + user).attr('class',choiceUpdate);}
+
   }
 
   // Update this players choice on the channel
@@ -122,7 +128,7 @@
     callStackReporter('playerChoiceUpdate()');
 
     RPSapp.pubNub.publish({
-      channel : "rps_channel",
+      channel : RPSapp.channelID,
       message : {
         user: RPSapp.thisUserID,
         choiceUpdate: RPSapp.playerChoice,
@@ -136,12 +142,22 @@
     callStackReporter('getCurrentChannelStatus()');
 
     RPSapp.pubNub.here_now({
-      channel : 'rps_channel',
+      channel : RPSapp.channelID,
       callback : function receiver( message ) {
+        // console.log(message);
+        if( message.occupancy >= 2 ) {
+
+          // Reset ID in storage
+          assignUniqueUserID();
+          window.location = window.location.origin;
+        }
         if( message.uuids.length > 0 ) {
+
+          // For Each User
           message.uuids.forEach(function(uuid){
             injectUser(uuid);
           });
+
         }
       }
     });
@@ -224,9 +240,38 @@
     Leap.loop(function(frame){
       if (RPSapp.gameRunning === true) {
         choiceDetector(frame);
-        updateGlobalTimer();
+        // updateGlobalTimer();
       }
     });
+  }
+
+  function createGameId(){
+    var rand = function() {
+        return Math.random().toString(36).substr(2); // remove `0.`
+    };
+    var token = function() {
+        return rand() + rand(); // to make it longer
+    };
+    window.location.hash = token();
+    return token();
+  }
+
+  // Start the clock
+  function roundOverDetection(){
+    callStackReporter('initClock');
+
+    RPSapp.gameRunning = false;
+    console.log('Round Over! You chose ' + RPSapp.playerChoice + '!');
+  }
+
+  // Publish to channel the time
+  function updateGlobalTimer() {
+    callStackReporter('updateGlobalTimer');
+  }
+
+  // For Testing only
+  function callStackReporter(funcName) {
+    console.log(funcName);
   }
 
   // Fire the whole app
